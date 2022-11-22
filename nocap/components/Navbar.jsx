bf584d727c525187984408801bfdb8e449d60390
ktrip96 <ktrip96@gmail.com>
2022-11-23 00:42:35 +0200

    Main page

---
+++ nocap/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai'
+
+import { Cart } from './';
+// import { useStateContext} from '../context/StateContext';
+
+const Navbar = () => {
+  return <h1>Navbar</h1>
+}
+
+export default Navbar
--- nocap/pages/_app.js
+++ nocap/pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import React from 'react'
+import { Layout } from '../components'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
