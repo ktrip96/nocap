fe2c300aa3078d6c7faff28b6fffa0e325f40af8
ktrip96 <ktrip96@gmail.com>
2022-11-25 00:26:38 +0200

    Sanity + Stripe

--- nocap/components/Cart.jsx
+++ nocap/components/Cart.jsx
@@ -6,31 +6,31 @@ import toast from 'react-hot-toast';
 
 import { useStateContext } from '../context/StateContext';
 import { urlFor } from '../lib/client';
-// import getStripe from '../lib/getStripe';
+import getStripe from '../lib/getStripe';
 
 const Cart = () => {
   const cartRef = useRef();
   const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuanitity, onRemove } = useStateContext();
 
-  // const handleCheckout = async () => {
-  //   const stripe = await getStripe();
+  const handleCheckout = async () => {
+    const stripe = await getStripe();
 
-  //   const response = await fetch('/api/stripe', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify(cartItems),
-  //   });
+    const response = await fetch('/api/stripe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cartItems),
+    });
 
-  //   if(response.statusCode === 500) return;
+    if(response.statusCode === 500) return;
     
-  //   const data = await response.json();
+    const data = await response.json();
 
-  //   toast.loading('Redirecting...');
+    toast.loading('Redirecting...');
 
-  //   stripe.redirectToCheckout({ sessionId: data.id });
-  // }
+    stripe.redirectToCheckout({ sessionId: data.id });
+  }
 
   return (
     <div className="cart-wrapper" ref={cartRef}>
@@ -75,7 +75,7 @@ const Cart = () => {
                     <span className="minus" onClick={() => toggleCartItemQuanitity(item._id, 'dec') }>
                     <AiOutlineMinus />
                     </span>
-                    <span className="num" onClick="">{item.quantity}</span>
+                    <span className="num">{item.quantity}</span>
                     <span className="plus" onClick={() => toggleCartItemQuanitity(item._id, 'inc') }><AiOutlinePlus /></span>
                   </p>
                   </div>
@@ -98,12 +98,10 @@ const Cart = () => {
               <h3>${totalPrice}</h3>
             </div>
             <div className="btn-container">
-              {/* <button type="button" className="btn" onClick={handleCheckout}>
+              <button type="button" className="btn" onClick={handleCheckout}>
                 Pay with Stripe
-              </button> */}
-              <button type="button" className="btn" >
-                Pay 
               </button>
+
             </div>
           </div>
         )}
--- nocap/components/Navbar.jsx
+++ nocap/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">JSM Headphones</Link>
+        <Link href="/">NoCap Eshop</Link>
       </p>
 
       <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
